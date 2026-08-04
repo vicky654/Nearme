190a86fcdcfd8e3855cd9190a0681f15928388b8
Vicky Verma <vickyverma2026@gmail.com>
2026-08-04 16:58:58 +0530

    Pacakge Update

--- server/src/utils/asyncHandler.ts
+++ server/src/utils/asyncHandler.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction, RequestHandler } from 'express';
 export function asyncHandler(
   fn: (req: Request, res: Response, next: NextFunction) => Promise<unknown>
 ): RequestHandler {
-  return (req, res, next) => {
+  return (req: Request, res: Response, next: NextFunction): void => {
     fn(req, res, next).catch(next);
   };
 }
